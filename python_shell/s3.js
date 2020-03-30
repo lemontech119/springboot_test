3fe44fdc5c4ba7e52de6d9674443b1d5ea22cdac
AhnDongwon <lemontech119@gmail.com>
2020-03-30 11:58:56 +0900

    Update s3.js

--- python_shell/s3.js
+++ python_shell/s3.js
@@ -6,6 +6,7 @@ const secretKey = process.env.SecretKey;
 const bucket = '';
 const s3 = new AWS.S3({ accessKeyId: key, secretAccessKey: secretKey });
 
+// s3 문서 다운로드
 const params = {
     Bucket: bucket,
     Key: `saemi/dataset/game_17/game_17_city.csv`
@@ -18,4 +19,19 @@ s3.getObject(params, (err, data)=>{
     console.log(data.Body.toString());
 })
 
-// s3 문서 다운로드
+// 리스트 조회 Prefix로 특정 디렉토리만 조회 가능
+const param = {
+    Bucket: 'haii-server-dev-serverlessdeploymentbucket-3bg9ru2dt3oy',
+    Prefix: 'saemi/dataset/game_18/'
+}
+
+var data = '';
+(async () => {
+    try {
+        const file = await s3.listObjects(param).promise();
+        console.log(file)
+
+    } catch (err) {
+        console.log(err);
+    }
+})();
